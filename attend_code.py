8728ef5c5968746a458731c232b5f5c8fe5fd3bc
FunnyBiu <549308442@qq.com>
2025-01-14 12:48:13 +0800

    Update attend_code.py

--- attend_code.py
+++ attend_code.py
@@ -19,7 +19,7 @@ headers = {
     'Sec-Fetch-Site': 'same-origin',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36 Edg/131.0.0.0',
     'X-Requested-With': 'XMLHttpRequest',
-    'access-token': '',
+    'access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MzY4MTM3NzAsInVzZXJuYW1lIjoiejIwMjIwMjMwODA0In0.jUakhE5yLP5kOSkzF2SpAIIrqeulEXwpGhr3jkWEc4w',
     'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -47,17 +47,17 @@ data=response.json()
 
 # 筛选 "className" 和 "courseName" 的记录
 filtered_ids = [
-    record["id"]
+     { "id" :record["id"],"day":record['cday']}
     for record in data["result"]["records"]
-    if record["className"] == "信安23-02" and record["courseName"] == "信息安全与代码审计"
+    if record["className"] == "信安23-01" and record["courseName"] == "Linux操作系统安全配置"
 ]
 
-print(filtered_ids)
+#print(filtered_ids)
 
 
-def course(id):
+def course(day,stu):
     response = requests.get(
-        f'https://newca.zjtongji.edu.cn/attendng/attend/m/teaRecord/getById/{id}?_t=1736737564790',
+        f'https://newca.zjtongji.edu.cn/attendng/attend/m/teaRecord/getById/{stu["id"]}?_t=1736737564790',
         cookies=cookies,
         headers=headers,
     )
@@ -80,8 +80,10 @@ def course(id):
     data=response.json()
     #print(json.dumps(data, ensure_ascii=False))
     for stu in data["result"]['2']:
-        print(stu['stuName']+'|'+stu['leaveReason'])
+        print(day+','+stu['stuName']+','+stu['leaveReason'])
     for stu in data["result"]['1']:
-        print(stu['stuName']+'|缺勤')
+        print(day+','+stu['stuName']+',缺勤')
+#print(len(filtered_ids))
+print("日期,姓名,原因")
 for stu in filtered_ids:
-    course(stu)
+    course(stu['day'],stu)
